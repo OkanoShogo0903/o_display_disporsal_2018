692514b09d52ef5c18525bb8e174c602155bb76b
demulab <robot.engineer.koyo@gmail.com>
2018-09-21 15:09:11 +0900

    launch起動のタイミングを調整

--- scripts/start.sh
+++ scripts/start.sh
@@ -20,15 +20,16 @@ callXterm() {
     xterm -geometry 80x100+0+0   -e "/opt/ros/kinetic/bin/roslaunch realsense2_camera rs_camera.launch" &
     sleep 10s
 
-    # ArUco marker.
+    ## ArUco marker.
     xterm -geometry 80x100+400+0   -e "/opt/ros/kinetic/bin/roslaunch aruco_ros marker_publisher.launch" &
-    sleep 3s
 
+    # Wheel.
+    xterm -geometry 80x100+0+0   -e "/opt/ros/kinetic/bin/rosrun wheel wheel" &
+    sleep 5s
+    
     # Master.
     xterm -geometry 80x100+800+0   -e "/opt/ros/kinetic/bin/roslaunch o_display_disporsal_2018 master.launch" &
 
-    # Wheel.
-    #xterm -geometry 80x100+0+0   -e "/opt/ros/kinetic/bin/roslaunch o_display_disporsal_2018 display_disporsal.launch" &
 }
 
 callGnome() {
